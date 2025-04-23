01a8e1fd172064c40cd35065dd8e6477f1ec66fa
Сидельников Артем <heavydisto@gmail.com>
2025-04-23 23:13:04 +0300

    Была реализована страница собеседования.

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from "@/components/Agent";
+
+const Page = () => {
+  return (
+    <>
+      <h3>Генерация собеседования</h3>
+
+      <Agent userName="Ты" userId="user1" type="generation" />
+    </>
+  );
+};
+
+export default Page;
---
+++ components/Agent.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import Image from "next/image";
+// import { useState, useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+// import { vapi } from "@/lib/vapi.sdk";
+// import { interviewer } from "@/constants";
+// import { createFeedback } from "@/lib/actions/general.action";
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
+}
+
+const agent = ({ userName }: AgentProps) => {
+  const callStatus = CallStatus.FINISHED;
+  const isSpeaking = true;
+  const messages = ["Как тебя зовут?", "Меня зовут Артем"];
+  const lastMessage = messages[messages.length - 1];
+
+  return (
+    <>
+      <div className="call-view">
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="vapi"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak" />}
+          </div>
+          <h3>AI Ассистент</h3>
+        </div>
+
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/user-avatar.png"
+              alt="user avatar"
+              width={539}
+              height={539}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
+          </div>
+        </div>
+      )}
+
+      <div className="w-full flex justify-center">
+        {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call" onClick={() => handleCall()}>
+            <span
+              className={cn(
+                "absolute animate-ping rounded-full opacity-75",
+                callStatus !== "CONNECTING" && "hidden"
+              )}
+            />
+
+            <span className="relative">
+              {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                ? "Call"
+                : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect" onClick={() => handleDisconnect()}>
+            Завершить
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default agent;
